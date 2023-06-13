118867c52591338a9806f0506da67de85c119bc5
ElenaVasilevna <kalugina.elena.84@mail.ru>
2023-06-13 21:49:31 +0300

    Созданы модели Categories, Genres, Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('-title',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Genres(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('-title',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Titles(models.Model):
+    name = models.CharField(          # название произведения
+        max_length=50,
+        verbose_name='Название произведения'
+    )
+    year = models.IntegerField(           # год
+        verbose_name='Год создания произведения'
+    )
+    category = models.ForeignKey(       # категория
+        Categories,
+        related_name='titles',
+        verbose_name='Категория произведения',
+    )
+    genre = models.ForeignKey(      # жанр
+        Genres,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Жанр произведения'
+    )
+    description = models.TextField(            # описаие
+        verbose_name='Описание произведения'
+    )
+
+    class Meta:
+        ordering = ('-year',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
