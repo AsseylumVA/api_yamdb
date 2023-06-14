052f246c220dc7e918783527cac8943ce2d84581
ElenaVasilevna <kalugina.elena.84@mail.ru>
2023-06-14 15:54:41 +0300

    Исправления после ревью:
    - исправлены имена полей в моделях Categories и Genres, к полям добавлены дополнительные аргументы
    - изменена настройка полей year, category, genre в моделе Titles
    - создана промежуточная модель для связи типа ManyToMany для произведений и жанров

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,54 +2,73 @@ from django.db import models
 
 
 class Categories(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    name = models.CharField(
+        max_length=50,
+        blank=False,
+        verbose_name='Наименование категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        blank=False
+    )
 
     class Meta:
-        ordering = ('-title',)
+        ordering = ('name',)
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
 
 
 class Genres(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    name = models.CharField(
+        max_length=50,
+        blank=False,
+        verbose_name='Название жанра'
+    )
+    slug = models.SlugField(
+        unique=True,
+        blank=False
+    )
 
     class Meta:
-        ordering = ('-title',)
+        ordering = ('name',)
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
 
 
 class Titles(models.Model):
-    name = models.CharField(          # название произведения
-        max_length=50,
+    name = models.CharField(
+        max_length=256,
+        blank=False,
         verbose_name='Название произведения'
     )
-    year = models.IntegerField(           # год
+    year = models.PositiveSmallIntegerField(  # Более подходящий тип поля - Значения от 0 до 32767
+        max_length=4,
+        blank=False,
         verbose_name='Год создания произведения'
     )
-    category = models.ForeignKey(       # категория
+    category = models.ForeignKey(
         Categories,
-        related_name='titles',
-        verbose_name='Категория произведения',
-    )
-    genre = models.ForeignKey(      # жанр
-        Genres,
-        blank=True,
+        blank=False,
         null=True,
         related_name='titles',
-        verbose_name='Жанр произведения'
+        verbose_name='Категория произведения',
+        on_delete=models.SET_NULL,
+
     )
-    description = models.TextField(            # описаие
+    genre = models.ManyToManyField(
+        Genres,
+        through='TitleGenre',
+        blank=False,
+        verbose_name='Описание жанра'
+    )
+    description = models.TextField(
+        blank=True,
         verbose_name='Описание произведения'
     )
 
@@ -60,3 +79,19 @@ class Titles(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TitleGenre(models.Model):
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE
+    )
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ('genre',)
+        verbose_name = 'Произведение и жанр'
+        verbose_name_plural = 'Произведения и жанры'
